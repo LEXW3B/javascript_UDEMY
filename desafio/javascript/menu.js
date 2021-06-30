33791358514df71e9119c4c60eca62d6c5cfcda6
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2021-06-30 19:47:38 -0300

    add

--- desafio/javascript/menu.js
+++ desafio/javascript/menu.js
@@ -1,11 +1,21 @@
 (function(){
+
     var $html = document.querySelector('html');
-    var $btn  = document.querySelector('.header-nav__hamburgger');
+    var $btn = document.querySelector('.header-nav__hamburgger');
+
+    var ariaControl = $btn.getAttribute('aria-controls');
+    
+    var $menu = document.getElementById(ariaControl);
 
     $html.classList.remove('no-js');
-    $html.classList.add('menu-opened');
+    $html.classList.add('js');
 
     $btn.addEventListener('click', function(){
         $html.classList.toggle('menu-opened');
+
+        var ariaExpanded = this.getAttribute('aria-expanded') === "true";
+
+        $btn.setAttribute('aria-expanded', !ariaExpanded);
+        $menu.setAttribute('aria-expanded', !ariaExpanded);
     })
 })()
